4965f99d1e7c366e8a00d8c191e4bc1709fa3919
Emmett Henderson <emmetth@cheesygames.com>
2020-07-22 10:18:34 -0400

    Added check(err) to simplify error checking Added const root for html directory

--- helpers.go
+++ helpers.go
@@ -8,54 +8,47 @@ import (
 	"path/filepath"
 )
 
+const root = "html"
+
+// panic if err is not nil
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 // help function that appends path to static html directory.
 func join_html(path string) string {
-	return filepath.Join("html", path)
+	return filepath.Join(root, path)
 }
 
 // Copies file into the static html directory.
 func Copy(src string, dest string) {
 	cmd := exec.Command("cp", src, join_html(dest))
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
+	check(cmd.Run())
 }
 
 // Create file in the static html directory and returns a file handle.
 func Create(path string) *os.File {
 	f, err := os.Create(join_html(path))
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 	return f
 }
 
 // Deletes all files and sub directories from the static html directory.
 func Empty() {
-	d, err := os.Open("html")
-	if err != nil {
-		panic(err)
-	}
+	d, err := os.Open(root)
+	check(err)
 	defer d.Close()
 
 	names, err := d.Readdirnames(-1)
-	if err != nil {
-		panic(err)
-	}
-
+	check(err)
 	for _, name := range names {
-		err = os.RemoveAll(join_html(name))
-		if err != nil {
-			panic(err)
-		}
+		check(os.RemoveAll(join_html(name)))
 	}
 }
 
 // Makes directory in the static html directory
 func Mkdir(path string) {
-	err := os.Mkdir(join_html(path), 0755)
-	if err != nil {
-		panic(err)
-	}
+	check(os.Mkdir(join_html(path), 0755))
 }
